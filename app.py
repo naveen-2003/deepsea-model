20d30f9dbdbce8f1c63d09353f3c508bb07c8544
Naveen N <nnaveen112233@gmail.com>
2024-11-07 15:42:30 +0530

    added inference functionality

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import shutil
 import os
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
+import cv2
 
 load_dotenv()
 
@@ -68,7 +69,7 @@ def train_model(name,organizationId, datasetId, datasetName, modelId, modelType,
     os.makedirs(os.path.dirname(destination_path), exist_ok=True)
     # db.models.delete_many({"isPreTrained":False,"dataset":ObjectId(datasetId),"baseModel":ObjectId(modelId),"organization": ObjectId(organizationId),"type":modelType})
     newModel = db.models.insert_one({ "name": name,"status":"training","isPreTrained":False,"dataset":ObjectId(datasetId),"baseModel":ObjectId(modelId),"organization": ObjectId(organizationId),"type":modelType,"isActive":True})
-    model.train(data="./data.yaml" if modelType!="classification" else "dataset" , epochs=10, imgsz=640, project="result/"+organizationId, name=f"{datasetName}_{modelType}_{modelId}", exist_ok=True)
+    model.train(data="./data.yaml" if modelType!="classification" else "dataset" , epochs=10,lr0=1e-4, imgsz=640,pretrained=True, project="result/"+organizationId, name=f"{datasetName}_{modelType}_{modelId}", exist_ok=True)
     shutil.rmtree(f"./dataset", ignore_errors=True)
     shutil.copy2(new_model_path, os.path.join(os.path.dirname(destination_path),f"{datasetName}_{modelType}_{modelId}.pt"))
     db.models.update_one({"_id":newModel.inserted_id},{"$set":{"status":"completed","path":f"public/models/{organizationId}/{datasetName}_{modelType}_{modelId}.pt"}})
@@ -99,7 +100,47 @@ def start_training():
 def check_training_status():
     return jsonify(training_status), 200
 
-# shutil.copy2("./result/66be16adf4fa57825b4cbd20/fishes_objectdetection_6703c8f627de77f27c257980/weights/best.pt","./result/66be16adf4fa57825b4cbd20/fishes_objectdetection_6703c8f627de77f27c257980/weights/best2.pt")
+@app.route('/inference', methods=['POST'])
+def inference_image():
+    json_data = request.get_json()
+    file = json_data['file']
+    modelId = json_data['modelId']
+
+    model = db.models.find_one({"_id":ObjectId(modelId)})
+    if model is not None and model['status'] == "completed" and model['isActive'] == True and file is not None:
+        print(model)
+        print(file)
+        modelPath = model['path']
+        if model['type'] == "segmentation":
+            yoloModel = YOLO(f"{os.getenv("STORAGE_PATH")}{modelPath}")
+            results = yoloModel(file['path'])
+            annotated_img = results[0].plot()
+            cv2.imwrite(file['path'], cv2.cvtColor(annotated_img, cv2.COLOR_RGB2BGR))
+            print( "results",results)
+            return jsonify({'file' : file}), 200
+        
+        if model['type'] == "objectdetection":
+            print(f"{os.getenv("STORAGE_PATH")}{modelPath}")
+            yoloModel = YOLO(f"{os.getenv("STORAGE_PATH")}{modelPath}")
+            print(yoloModel)
+            # yoloModel = YOLO(f"yolov8n.pt")
+            results = yoloModel(file['path'])
+            annotated_img = results[0].plot()
+            cv2.imwrite(file['path'], cv2.cvtColor(annotated_img, cv2.COLOR_RGB2BGR))
+            print( "results",results)
+            return jsonify({'file' : file}), 200
+        
+        if model['type'] == "classification":
+            yoloModel = YOLO(f"{os.getenv("STORAGE_PATH")}{modelPath}")
+            # model = YOLO("yolov8n-cls.pt")
+            results = yoloModel(file['path'])
+            print("results",results[0])
+            probs = results[0].probs  # get classification probabilities
+            top1_confidence = probs.top1
+            classifications = results[0].names
+            first_key = list(classifications.keys())[top1_confidence]
+            first_value = classifications[first_key]
+            return jsonify({'classname' : first_value}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
